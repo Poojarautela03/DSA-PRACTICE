0d71e1f14f4dc0fa03eb9d2ff40ad63ee8812c63
Poojarautela03 <poojaraute03@gmail.com>
2026-06-11 10:23:42 +0530

    Delete circle.c

--- circle.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-//wap to find area of circe
-int main (){
-    float radius;
-    printf("enter radius");
-    scanf("%f",&radius);
-    printf("area of circle:%f",3.14*radius*radius);
-    return 0;
-}
